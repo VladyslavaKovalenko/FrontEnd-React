9d3c07bacb3316a0b62c7be74b56c22a68fe06ff
Vladyslava Kovalenko <vladakovalenko1903@gmail.com>
2023-05-24 18:57:31 +0200

    HW1: JSX, Props, State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import Child from './Child';
 
-function App() {
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      name: 'Stepan',
+      age: 25,
+      isShow: false,
+      buttonText: 'show'
+    }
+  }
+
+// при нажатии 'Stepan' поменяется на 'Mykola', а 25 поменяется на 30.
+  setAge=()=>{
+    this.setState({name: 'Mykola', age:30})
+  }
+
+  changeState=()=>{
+    this.setAge()
+    this.setState({isShow: !this.state.isShow})
+    this.state.buttonText==='show'?this.setState({buttonText: 'hide'}):
+    this.setState({buttonText: 'show'})
+  }
+
+  render(){
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HW1 JSX, Props, State</h1>
+        {this.state.isShow && <p>Name: {this.state.name}, age: {this.state.age}</p>}
+        <button onClick={this.changeState}>{this.state.buttonText}</button>
     </div>
+
+    </>
   );
+  }
 }
 
 export default App;
