49f39460d65c88f79579f187d343380eee04c863
Vladyslava Kovalenko <vladakovalenko1903@gmail.com>
2023-05-29 17:00:42 +0200

    HW: Lifecycle hooks

--- src/Child.js
+++ src/Child.js
@@ -1,13 +1,39 @@
 import React from 'react';
 
 class Child extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+       name: 'Stepan',
+      age: 25,
+      isShow: false,
+      buttonText: 'show'
+    }
+  }
+  //  при нажатии 'Stepan' поменяется на 'Mykola', а 25 поменяется на 30.
+  setAge=()=>
+  {   this.setState({name: 'Mykola', age:30})
+  }
+
+  changeState=()=>{
+    this.setAge()
+    this.setState({isShow: !this.state.isShow})
+    this.state.buttonText==='show'?this.setState({buttonText: 'hide'}):
+    this.setState({buttonText: 'show'})
+  }
   render(){
-    // console.log(this)
+
   return (
     <>
     <div className="App">
       
-      
+    <div className="App">
+      <h1>HW1 JSX, Props, State</h1>
+        {this.state.isShow && <p>Name: {this.state.name}, age: {this.state.age}</p>}
+        <button onClick={this.changeState}>{this.state.buttonText}</button>
+    </div>
+
+  
 
     </div>
     </>
